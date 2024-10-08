93965380ae28544f1d2139e03fac934bd6fa1682
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2024-10-08 09:31:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ let MenuToggle = document.getElementById("menu-toggle");
 
 MenuToggle.addEventListener("click", function (e) {
   document.querySelector("body").classList.toggle("mobile-nav-active");
-  this.classList.toggle('fa-xmark')
+  this.classList.toggle("fa-xmark");
+});
+
+// dynamic auto slideing text
+let typed = new Typed(".slide-text", {
+  strings: ["FullStack Developer", "Ui/Ux Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  backDelay: 2000,
 });
